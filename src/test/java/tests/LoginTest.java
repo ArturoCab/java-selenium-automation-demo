e55cc13ebe6ec2008fbd3b124e2ee86aec64518e
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-02-06 13:59:44 -0600

    Test username and password

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,9 +1,11 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginTest{
     WebDriver driver;
@@ -15,6 +17,16 @@ public class LoginTest{
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
+    @Test
+    void sucessfulLogin(){
+        driver.get("https://the-internet.herouapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        WebElement mensaje = driver.findElement(By.id("flash"));
+        assertTrue(mensaje.getText().contains("You logged into a secure area!"));
+    }
 
     @AfterEach 
     void tearDown(){
